a00e899252eff2fb0210b92b16ed4508b363f81c
sinius15 <sinius15@gmail.com>
2014-02-11 15:26:18 +0100

    first test succesfull

--- src/com/sinius15/pamapapi/PrisonLoader.java
+++ src/com/sinius15/pamapapi/PrisonLoader.java
@@ -1,23 +1,61 @@
 package com.sinius15.pamapapi;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class PrisonLoader {
 	private static String syncLock = "teamMath";
 	
-	public static Prison loadPrison(File file){
+	public static Prison loadPrison(File file) throws IOException{
+		if(!file.exists())
+			throw new FileNotFoundException("[sqrerror] File Not Found: "+file.getAbsolutePath());
 		synchronized (syncLock) {
+			
 			Prison out = new Prison();
 			out.sourceFile = file;
 			out.prisonName = file.getName().replace(".prison", "");
 			
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			System.out.println(countLines(file));
+			String line;
+			while ((line = br.readLine()) != null) {
+				
+			}
+			br.close();
 			
 			
 			
 			
-			
+			return out;
 		}
-		return null;
+		
+	}
+	
+	public static int countLines(File file) throws IOException {
+	    InputStream is = new BufferedInputStream(new FileInputStream(file));
+	    try {
+	        byte[] c = new byte[1024];
+	        int count = 0;
+	        int readChars = 0;
+	        boolean empty = true;
+	        while ((readChars = is.read(c)) != -1) {
+	            empty = false;
+	            for (int i = 0; i < readChars; ++i) {
+	                if (c[i] == '\n') {
+	                    count++;
+	                }
+	            }
+	        }
+	        return (count == 0 && !empty) ? 1 : count+1;
+	    } finally {
+	        is.close();
+	    }
 	}
 	
 }
---
+++ src/com/sinius15/pamapapi/test/Test.java
@@ -0,0 +1,15 @@
+package com.sinius15.pamapapi.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.sinius15.pamapapi.Prison;
+import com.sinius15.pamapapi.PrisonLoader;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException {
+		Prison p = PrisonLoader.loadPrison(new File("C:\\Users\\Sijmen\\Desktop\\test.prison"));
+	}
+	
+}
